3ed6d772990c1ab0b0917b88d9e4df5667b72d4c
张复星 <502545703@qq.com>
2020-12-04 23:44:59 +0800

    add code

--- index.d.ts
+++ index.d.ts
@@ -1,53 +1,42 @@
-# Multi Progress Bar
-multi-line progress bar output in terminal written in nodejs
+// Type definitions
 
-## Install
+interface ProgressBarOptions {
+  /**
+   * Progress bar title, default: ''
+   */
+  title?: string;
 
-```sh
-$ npm i @zhangfuxing/multi-progress
-```  
+  /**
+   * tthe displayed width of the progress, default: 50
+   */
+  width?: number;
 
-## Useage  
+  /**
+   * completion character, default: colors.bgGreen(' '), can use any string
+   */
+  complete?: string;
 
-```js
-const ProgressBar = require('@zhangfuxing/multi-progress');
+  /**
+   * incomplete character, default: colors.bgWhite(' '), can use any string
+   */
+  incomplete?: string;
 
-const title = 'download files';
-const total = 100;
+  /**
+   * minimum time between updates in milliseconds, default: 16
+   */
+  interval?: number;
 
-const bars = new ProgressBars({
-  title,
-  // clear: true,
-  complete: '*',
-  incomplete: '.',
-  display: '[:bar] :text :percent :time :completed/:total'
-});
+  /**
+   * What is displayed and display order, default: ':bar :text :percent :time :completed/:total'
+   */
+  display?: string;
 
-let completed1 = 0;
-let completed2 = 0;
-
-function downloading() {
-  if (completed1 <= total || completed2 <= total) {
-    completed1 += 1
-    completed2 += 2
-    bars.render([
-      { completed: completed1, total, text: "file1" },
-      { completed: completed2, total, text: "file2" }
-    ]);
-
-    setTimeout(function () {
-      downloading();
-    }, 100)
-  }
+  /**
+   * clear the bar on completion, default: false
+   */
+  clear?: boolean;
 }
 
-downloading();
-```  
-
-More examples in the `examples` folder.
-
-## Type definitions
-```ts
 declare class ProgressBars {
   /**
    * Options:
@@ -81,18 +70,5 @@ declare class ProgressBars {
    */
   end(): void;
 }
-```  
 
-## Screenshots
-
-![default](./screenshots/default.gif) 
-
-![complete](./screenshots/complete.gif) 
-
-![display](./screenshots/display.gif) 
-
-![console](./screenshots/console.gif)  
-
-![clear](./screenshots/clear.gif)  
-
-More screenshots in the `screenshots` folder.
+export = ProgressBars
---
+++ index.js
@@ -0,0 +1,154 @@
+/*
+ * ProgressBars
+ */
+const colors = require('@zhangfuxing/colors/fn');
+
+class ProgressBars {
+  #end = false;
+  #lastRows = 1;
+  #strs = [];
+  #startIndex = 0;
+
+  /**
+   * constructor
+   * 
+   * @param {String} [title] Progress bar title, default: ''
+   * @param {Number} [width] the displayed width of the progress, default: 50
+   * @param {String} [complete] completion character, default: colors.bgGreen(' '), can use any string
+   * @param {String} [incomplete] incomplete character, default: colors.bgWhite(' '), can use any string
+   * @param {Boolean} [clear]  clear the bar on completion, default: false
+   * @param {Number} [interval]  minimum time between updates in milliseconds, default: 16
+   * @param {String} [display]  What is displayed and display order, default: ':bar :text :percent :time :completed/:total'
+   */
+  constructor({ title = '', width, complete, incomplete, clear, interval, display } = {}) {
+    this.width = width || 50;
+    this.complete = complete || colors.bgGreen(' ');
+    this.incomplete = incomplete || colors.bgWhite(' ');
+    this.clear = clear || false;
+    this.interval = interval || 16;
+    this.display = display || ':bar :text :percent :time :completed/:total';
+    this.stream = process.stdout;
+    this.lastStr = '';
+    this.start = Date.now();
+    this.time = 0;
+    this.lastRender = 0;
+
+    if (title != '') {
+      this.#strs.push(title);
+      this.#startIndex = 1;
+    }
+
+    if (typeof title !== 'string') throw new Error(`title must be 'string'`);
+    if (typeof this.width !== 'number') throw new Error(`width must be 'number'`);
+    if (typeof this.complete !== 'string') throw new Error(`complete must be 'string'`);
+    if (typeof this.incomplete !== 'string') throw new Error(`incomplete must be 'string'`);
+    if (typeof this.clear !== 'boolean') throw new Error(`clear must be 'boolean'`);
+    if (typeof this.interval !== 'number') throw new Error(`interval must be 'number'`);
+    if (typeof this.display !== 'string') throw new Error(`display must be 'string'`);
+  }
+
+  /**
+   * "render" the progress bar
+   * 
+   * @param {Object[]} bars Progress Bars
+   * @param {Number} bars[].completed completed value
+   * @param {Number} [bars[].total] total number of ticks to complete, default: 100
+   * @param {String} [bars[].text] text displayed per ProgressBar, default: ''
+   * 
+   * @api public
+   */
+  render(bars) {
+    if (this.#end || !this.stream.isTTY) return;
+
+    const now = Date.now();
+    const ms = now - this.lastRender;
+    this.lastRender = now;
+    this.time = ((now - this.start) / 1000).toFixed(1) + 's';
+    let end = true;
+    let index = this.#startIndex;
+    for (let { completed, total = 100, text = "" } of bars) {
+      completed = +completed;
+      if (!Number.isInteger(completed)) throw new Error(`completed must be 'number'`);
+      if (completed < 0) throw new Error(`completed must greater than or equal to 0`);
+      if (!Number.isInteger(total)) throw new Error(`total must be 'number'`);
+      if (completed > total && this.#strs[index] != undefined) continue;
+      end = false;
+      const percent = ((completed / total) * 100).toFixed(2) + '%';
+
+      // :bar :text :percent :time :completed/:total
+      let str = this.display
+        .replace(':text', text)
+        .replace(':time', this.time)
+        .replace(':percent', percent)
+        .replace(':completed', completed)
+        .replace(':total', total);
+
+      // compute the available space (non-zero) for the bar
+      let availableSpace = Math.max(0, this.stream.columns - str.replace(':bar', '').length);
+      if (availableSpace && process.platform === 'win32') availableSpace -= 1;
+
+      const width = Math.min(this.width, availableSpace);
+
+      // :bar
+      const completeLength = Math.round(width * completed / total);
+      const complete = new Array(completeLength).fill(this.complete).join('');
+      const incomplete = new Array(width - completeLength).fill(this.incomplete).join('');
+
+      str = str.replace(':bar', complete + incomplete);
+      this.#strs[index++] = str;
+    }
+    if (ms < this.interval && end == false) return;
+    const renderStr = this.#strs.join("\n") + "\x1b[?25l";
+
+    if (this.lastStr !== renderStr) {
+      this.stream.moveCursor(0, -this.#lastRows + 1);
+      this.stream.cursorTo(0);
+      this.stream.clearScreenDown();
+      this.stream.write(renderStr);
+      this.lastStr = renderStr;
+      this.#lastRows = this.#strs.length;
+    }
+
+    if (end) this.end();
+  }
+
+  /**
+   * end progress bars.
+   * 
+   * @api public
+   */
+  end() {
+    this.#end = true;
+
+    if (this.clear) {
+      this.stream.moveCursor(0, -this.#lastRows + 1);
+      this.stream.cursorTo(0);
+      this.stream.clearScreenDown();
+    } else {
+      this.stream.write('\n');
+    }
+    this.stream.write('\x1b[?25h');
+  }
+
+  /**
+   * interrupt the progress bar and write a message above it
+   * 
+   * @param {string | number} message The message to write
+   * 
+   * @api public
+   */
+  console(message) {
+    // clear the current line
+    this.stream.clearLine(0);
+    // move the cursor to the start of the line
+    this.stream.cursorTo(0);
+    // write the message text
+    this.stream.write(`${message}`);
+    // wrap after writing the message
+    this.stream.write('\n');
+    // re-display the progress bar with its lastStr
+    this.stream.write(this.lastStr);
+  };
+}
+
+module.exports = ProgressBars
